1bd69365da104990bf340fd43fea68c3e4cab8ba
iolemenezess <iolemenezess@gmail.com>
2025-10-14 21:51:15 -0300

    primeira aplicacao

---
+++ core/aluno/aluno_controller.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request, jsonify
+from core.aluno.aluno_service import AlunoService
+from core.aluno.aluno import Aluno
+
+aluno_service = AlunoService()
+
+aluno_controller = Blueprint('aluno', __name__, url_prefix='/alunos')
+
+
+# criar decoreitor
+@aluno_controller.route('/', methords=['GET'])
+def listar_alunos():
+    alunos = aluno_service.listar_alunos()
+    return jsonify(alunos)
+
+@aluno_controller.route('/', methods=['POST'])
+def adicinar_aluno():
+    dados = request.get_json()
+    obj_aluno = Aluno(id=0,nome=dados["nome"])
+    aluno_service.adicionar_aluno(obj_aluno)
---
+++ core/aluno/aluno_repository.py
@@ -0,0 +1,13 @@
+from core.aluno.aluno_repository import AlunoRepository
+
+
+
+class AlunoRepository:
+    def __init__(self, db_name=""):
+        self.db_name = db_name
+
+    def listar(self):
+        pass
+    
+    def adicionar(self, aluno):
+        pass
